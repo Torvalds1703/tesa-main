57e04196d6f2aa9f6b4892cafcd6f02e238a5911
Torvalds1703 <isaevtim4@yandex.ru>
2024-09-15 23:41:56 +0700

    Initial commit

---
+++ src/main/java/team/project/practice/tesa_main/TesaMainApplication.java
@@ -0,0 +1,13 @@
+package team.project.practice.tesa_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesaMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesaMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/project/practice/tesa_main/TesaMainApplicationTests.java
@@ -0,0 +1,13 @@
+package team.project.practice.tesa_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesaMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
